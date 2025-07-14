3801276a31e546b0711426540d9666d97a408b5c
Joseph Ahor Abandoh-Sam <abandoh@gmail.com>
2025-07-14 02:14:23 +0000

    created file for basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,7 @@
+number1 = 10
+number2 = 5
+
+print("Addition of {} and {} is {}".format(number1,number2, (number1+number2)))
+print("Subtraction of {} and {} is {}".format(number1,number2, (number1 - number2)))
+print("Multiplication of {} and {} is {}".format(number1,number2, (number1 * number2)))
+print("Division of {} and {} is {}".format(number1,number2, (number1 / number2)))
